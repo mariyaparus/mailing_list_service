cb2666bf7752211d7066494a4ec56b0c791a69ff
MariyaParus <cat-she@mail.ru>
2024-06-27 17:21:05 +0300

    update users admin.py

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,6 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email','first_name', 'last_name',)
+    list_display = ('email','first_name', 'last_name', 'is_active')
     list_filter = ('email',)
     search_fields = ('first_name', 'last_name',)
