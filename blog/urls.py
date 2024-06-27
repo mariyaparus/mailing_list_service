3c28162bab3378603daacf7a8eaaedb31e16b5c8
MariyaParus <cat-she@mail.ru>
2024-06-27 17:16:47 +0300

    update blog urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from blog.apps import BlogConfig
 from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
@@ -7,7 +8,7 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('blog/', BlogListView.as_view(), name='blog'),
-    path('blog/<int:pk>/', BlogDetailView.as_view(), name='article'),
+    path('blog/<int:pk>/', cache_page(60)(BlogDetailView.as_view()), name='article'),
     path('blog/create/', BlogCreateView.as_view(), name='blog_create'),
     path('blog/update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
     path('blog/delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
